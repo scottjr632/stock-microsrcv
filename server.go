612811cb4c9fbf8d6a3b5771ab093a6c104fbfd3
Scott Richardson <scottjr632@gmail.com>
2019-01-19 19:51:48 +0000

    ..

--- server.go
+++ server.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"stock-microsrvc/routers"
 )
 
 func main() {
 
-	port := ":8080"
+	port, ok := os.LookupEnv("STOCK_PORT")
+	if !ok {
+		port = "8080"
+	}
 
 	router := routers.NewRouter()
 	fmt.Printf("Server running on port %v\n", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
