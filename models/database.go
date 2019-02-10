a4fbe3e5960e7c5038f60ac8d69297c796214c57
Scott Richardson <scottjr632@gmail.com>
2019-02-10 06:28:34 +0000

    ...

--- models/database.go
+++ models/database.go
@@ -12,17 +12,20 @@ import (
 const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "postgres"
-	DB_HOST     = "192.168.1.21"
-	DB_NAME     = "testdb"
-	DB_PORT     = "5434"
+	DB_HOST     = "localhost"
+	DB_NAME     = "postgres"
+	DB_PORT     = "5432"
 )
 
 // executeQuery create a new Postgres DB insance and executes a query.
 // It defers db.Close(). This method should only be used for executing
 // querys and not executing any DML.
 func executeQuery(query string) (*sql.Rows, error) {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s port=%s host=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST)
+	dbinfo := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		dbinfo = fmt.Sprintf("user=%s password=%s dbname=%s port=%s host=%s sslmode=disable",
+			DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST)
+	{
 	db, err := sql.Open("postgres", dbinfo)
 	utils.CheckErr(err)
 	defer db.Close()
